b8f0e3890a1abab1565981b38365dd4c50f56730
Michael Gimbel <gimbel0893@gmail.com>
2014-01-17 18:59:12 -0500

    feature-rds-getri, updated class and got call working on test account that has an ri

--- boto/rds/dbri.py
+++ boto/rds/dbri.py
@@ -1,10 +1,58 @@
-class ReservedDBInstance(dict):
+import datetime
 
-    def __setattr__(self, attr, value):
-        self[attr] = value
 
-    def startElement(self, name, attrs, connection):
-        pass
+class RecurringCharge(object):
+
+    def __init__(self, recurring_charge_amount=None,
+                 recurring_charge_frequency=None):
+        self.recurring_charge_amount = recurring_charge_amount
+        self.recurring_charge_frequency = recurring_charge_frequency
+
+class ReservedDBInstance(object):
+
+    ATTR_MAP = {
+        'ReservedDBInstanceId': ('id', str),
+        'CurrencyCode': ('currency_code', str),
+        'DBInstanceClass': ('db_instance_class', str),
+        'DBInstanceCount': ('db_instance_count', int),
+        'Duration': ('duration', int),
+        'FixedPrice': ('fixed_price', float),
+        'MultiAZ': ('multi_az', bool),
+        'OfferingType': ('offering_type', str),
+        'ProductDescription': ('product_description', str),
+        'ReservedDBInstancesOfferingId': ('reserved_db_instances_offering_id',
+                                          str),
+        'StartTime': ('start_time',
+         lambda v: datetime.datetime.strptime(v[:-5], '%Y-%m-%dT%H:%M:%S')),
+        'State': ('state', str),
+        'UsagePrice': ('usage_price', float),
+    }
+
+    def __init__(self, connection=None, id=None):
+        self.connection = connection
+        self.id = id
+        self._extra = {}
+
+    def __repr__(self):
+        repr = ''
+        for var in vars(self):
+            attr = getattr(self, var)
+            repr += "{}={} [{}]\n".format(var, attr, str(type(attr)))
+        return repr
+
+    def startElement(self, name, attrs, connection): pass
 
     def endElement(self, name, value, connection):
-        setattr(self, name, value)
+        if name == 'RecurringCharges':
+            rc = []
+            for charge in value:
+                r = RecurringCharge(charge['RecurringChargeAmount'],
+                                    charge['RecurringChargeFrequency'])
+                rc.append(r)
+            setattr(self, 'recurring_charges', rc)
+        elif name in self.ATTR_MAP:
+            name, type = self.ATTR_MAP[name]
+            value = type(value)
+            setattr(self, name, value)
+        else:
+            self._extra[name] = value
